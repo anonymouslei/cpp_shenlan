652d351d99fe25bdef17f5d1959a10b6194c8286
Lei Ge <gracegl2014@gmail.com>
2021-10-28 21:39:53 +0200

    change int to unsigned int

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <iterator>
 
-template <int... valsT> struct iseq {};
+template <unsigned int... valsT> struct iseq {};
 
-template <int... valsT> void print(iseq<valsT...>) {
-  static constexpr int vals[] = {valsT...};
+template <unsigned int... valsT> void print(iseq<valsT...>) {
+  static constexpr unsigned int vals[] = {valsT...};
   std::copy(std::begin(vals), std::end(vals),
             std::ostream_iterator<int>(std::cout, " "));
   std::endl(std::cout);
 }
 
 // reverse
-// template<typename Res, typename Rem> struct Reverse;
+template<typename Res, typename Rem> struct Reverse;
 
-// template<int U1> struct Reverse<>;
+// template<int T, int Remain> struct Reverse<iseq<>, iseq<T, TRemain...>>;
 
 
 // add
 template<typename x1, typename x2, typename x3> struct Add;
 
-template<int x1, int x2, int sign>
+template<unsigned int x1, unsigned int x2, unsigned int sign>
 struct Add<iseq<x1>, iseq<x2>,iseq<sign>> {
-    constexpr static int res = x1 + x2 + sign;
+    constexpr static unsigned int res = x1 + x2 + sign;
 };
 
 int main() {
