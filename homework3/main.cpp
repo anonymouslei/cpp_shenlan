8cc879059ad28bac3594b474d7c771828389a31d
Lei Ge <gracegl2014@gmail.com>
2021-10-30 21:36:26 +0200

    add func

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -10,7 +10,7 @@ template <unsigned int... valsT> void print(iseq<valsT...>) {
   std::endl(std::cout);
 }
 
-// reverse, //写的不对
+// reverse
 template <typename Res, typename Rem> struct Reverse;
 
 // 第一种情况：前面的数组为0，后面的数组有值。将后面的数字的第一位放到前面的数组
@@ -39,24 +39,73 @@ template <unsigned int x1, unsigned int x2, unsigned int sign>
 struct Add_item<iseq<x1>, iseq<x2>, iseq<sign>>
     : std::conditional<((x1 + x2 + sign) < 10), iseq<x1 + x2 + sign>,
                        iseq<x1 + x2 + sign - 10>>::type {
-  static constexpr unsigned int tmp = x1 + x2 + sign;
-  using new_sign = iseq<(tmp / 10)>;
+  static constexpr unsigned add_value = x1+x2+sign;
+  static constexpr unsigned int value = (add_value) % 10;
+  using new_sign = iseq<(add_value / 10)>;
 };
 
+// add func
+template <typename res, typename x1, typename x2, typename sign> struct Add;
+
+// 第一种情况：刚开始计算，结果为空， x1和x2有数值，符号位为0
+template <unsigned int T1, unsigned int... TRmain1, unsigned int T2,
+          unsigned int... TRmain2>
+struct Add<iseq<>, iseq<T1, TRmain1...>, iseq<T2, TRmain2...>, iseq<0>()> {
+  using tmp = Add_item<iseq<T1>, iseq<T2>, iseq<0>>;
+  using res = typename Add<tmp, iseq<TRmain1...>, iseq<TRmain2...>,
+                           typename tmp::new_sign>::res;
+};
+
+// 第二种情况：结果不为空，x1为空
+template <unsigned int... TRes, unsigned int T2, unsigned int... TRmain2,
+          unsigned int sign>
+struct Add<iseq<TRes...>, iseq<>, iseq<T2, TRmain2...>, iseq<sign>> {
+  using res = iseq<TRes..., T2 + sign, TRmain2...>;
+};
+
+// 第三种情况：结果不为空，x2为空
+template <unsigned int... TRes, unsigned int T1, unsigned int... TRmain1,
+          unsigned int sign>
+struct Add<iseq<TRes...>, iseq<T1, TRmain1...>, iseq<>, iseq<sign>> {
+  using res = iseq<TRes..., T1 + sign, TRmain1...>;
+};
+
+// 第四种情况:结果不为空，x1,x2为空,//需要根据符号位是否为0来判断
+template <unsigned int... TRes, 
+          unsigned int sign>
+struct Add<iseq<TRes...>, iseq<>, iseq<>, iseq<sign>>
+    : std::conditional<(sign == 1), iseq<TRes..., sign>, iseq<TRes...>>::type {
+};
+
+// 第五种情况：结果不为空，x1,x2也不为空
+template <unsigned int... TRes, unsigned int T1, unsigned int... TRmain1, unsigned int T2,
+          unsigned int... TRmain2, unsigned int sign>
+struct Add<iseq<TRes...>, iseq<T1, TRmain1...>, iseq<T2, TRmain2...>, iseq<sign>> {
+  using tmp = Add_item<iseq<T1>, iseq<T2>, iseq<sign>>;
+  using res = typename Add<iseq<TRes..., tmp::value>, iseq<TRmain1...>, iseq<TRmain2...>,
+                           typename tmp::new_sign>::res;
+};
 
 int main() {
 
   // constexpr auto x = Add<iseq<2>, iseq<4>, iseq<0>>::res;
-  using D1 = Add_item<iseq<9>, iseq<2>, iseq<1>>;
-  print(D1());
-  print(D1::new_sign());
+  // using D1 = Add_item<iseq<9>, iseq<2>, iseq<1>>;
+  // print(D1());
+  // print(D1::new_sign());
 
-  using D2 = Add_item<iseq<0>, iseq<0>, iseq<0>>;
-  print(D2());
-  print(D2::new_sign());
-  // std::cout << x << std::endl;
-  using D = iseq<1, 5, 8, 2, 9>;
-  print(D());
-  using res = Reverse<iseq<>, D>::old_seq;
+  // using D2 = Add_item<iseq<0>, iseq<0>, iseq<0>>;
+  // print(D2());
+  // print(D2::new_sign());
+  // // std::cout << x << std::endl;
+  // using D = iseq<1, 5, 8, 2, 9>;
+  // print(D());
+  // using res = Reverse<iseq<>, D>::old_seq;
+  // print(res());
+
+  using D1 = iseq<8,3,2>;
+  using D2 = iseq<5>;
+  using res = Add<iseq<>, D1, D2, iseq<0>>::res;
   print(res());
+
+
 }
